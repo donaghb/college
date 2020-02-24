3149682a9add23989356afb2075f4d8816b293aa
donaghb <44291832+donaghb@users.noreply.github.com>
2020-02-24 22:20:34 +0000

    housing_tenure_example

---
+++ data_analytics/household_tenure.py
@@ -0,0 +1,9 @@
+import csv 
+
+with open('Practical/housing_tenure.csv', 'r') as file:
+    reader = csv.DictReader(file,delimiter=',')
+    for row in reader: 
+		geogID = row["GEOG_ID"]
+        county = row["County_UD"]
+		ownerOccupier = row["Owner Occupier with Mortgage (Households)"]
+        print("Total number of owner occupiers in the region of {}, in the county of {} is {}".format(geogID, county, ownerOccupier))
